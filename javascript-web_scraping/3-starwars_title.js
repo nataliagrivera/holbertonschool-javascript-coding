f752fe910495c36d7d01406927d2921476da122f
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-25 19:32:07 -0400

    Add script for retrieving Star Wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+//Check if the URL is a provided argument
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 3-starwars_title.js <movie-id>');
+  process.exit(1);
+}
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+// Make a request to get the Start Ward api
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1); 
+  }
+
+  if(response.statusCode === 200) { // Response was successful
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  } else {
+    console.error('Request failed');
+    process.exit(1);
+  }
+});
