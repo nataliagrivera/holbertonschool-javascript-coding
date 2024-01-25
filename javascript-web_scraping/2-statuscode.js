50d2dc07fc42dac1fd4b364c55758fde2917a138
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-25 15:31:08 -0400

    Fix error handling in GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -11,11 +11,11 @@ if(process.argv.length !== 3) {
 const url = process.argv[2];
 
 // Print the status code of a GET request to the provided URL
-request(url, (err, response)) => {
+request(url, (err, response) => {
   if (err) {
-    console.error(error);
+    console.error(err);
     process.exit(1); // Exit with an error code
   }
-}
 
 console.log('code: ${response.statusCode}');
+});
