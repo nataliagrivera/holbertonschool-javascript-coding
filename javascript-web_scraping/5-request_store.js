8b50ec7cbf54896c0e9c51f99531f264e5d0e5a9
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-26 00:48:39 -0400

    Refactor print statement and update comment

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -9,7 +9,7 @@ const file = process.argv[3];
 request(url, function (err, response, body) {
   if (err) { // Print the error if one occurred
     console.error(err);
-  } else { // Print
+  } else { // Print content of the response (body)
     fs.writeFile(file, body, 'utf8', (err) => {
       if (err) {
         console.error(err);
